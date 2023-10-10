01612a1412f8554f47ca66e7a5703b76ea9ccb96
Serena-Anthony <2003serio@gmail.com>
2023-10-10 10:04:10 +0530

    stage 6 first commit

--- Schema/Schema.cpp
+++ Schema/Schema.cpp
@@ -1,6 +1,8 @@
 #include "Schema.h"
 #include <cmath>
-#include <cstring>
+#include <string.h>
+#include<stdio.h>
+
 
 //---------stage5-----------
 int Schema::openRel(char relName[ATTR_SIZE]) {
@@ -34,4 +36,49 @@ int Schema::closeRel(char relName[ATTR_SIZE]) {
   }
 
   return OpenRelTable::closeRel(relId);
+}
+
+
+//-------stage6-------
+
+int renameRel(char oldRelName[ATTR_SIZE], char newRelName[ATTR_SIZE]) {
+    // if the oldRelName or newRelName is either Relation Catalog or Attribute Catalog,
+        // return E_NOTPERMITTED
+        // (check if the relation names are either "RELATIONCAT" and "ATTRIBUTECAT".
+        // you may use the following constants: RELCAT_NAME and ATTRCAT_NAME)
+
+  if(strcmp(oldRelName, RELCAT_RELNAME)==0 || strcmp(oldRelName, ATTRCAT_RELNAME)==0||strcmp(newRelName, RELCAT_RELNAME)==0 || strcmp(newRelName, ATTRCAT_RELNAME)==0)
+  {
+    return E_NOTPERMITTED;
+  }
+    // if the relation is open
+    //    (check if OpenRelTable::getRelId() returns E_RELNOTOPEN)
+    //    return E_RELOPEN
+if(OpenRelTable::getRelId()==E_RELNOTOPEN)
+{
+  return E_RELNOTOPEN;
+}
+    int retVal = BlockAccess::renameRelation(oldRelName, newRelName);
+     return retVal;
+}
+
+int Schema::renameAttr(char *relName, char *oldAttrName, char *newAttrName) {
+    // if the relName is either Relation Catalog or Attribute Catalog,
+        // return E_NOTPERMITTED
+        // (check if the relation names are either "RELATIONCAT" and "ATTRIBUTECAT".
+        // you may use the following constants: RELCAT_NAME and ATTRCAT_NAME)
+ if(strcmp(relName, RELCAT_RELNAME)==0 || strcmp(relName, ATTRCAT_RELNAME)==0)
+  {
+    return E_NOTPERMITTED;
+  }
+    // if the relation is open
+        //    (check if OpenRelTable::getRelId() returns E_RELNOTOPEN)
+        //    return E_RELOPEN
+if(OpenRelTable::getRelId()==E_RELNOTOPEN)
+{
+  return E_RELNOTOPEN;
+}
+    // Call BlockAccess::renameAttribute with appropriate arguments.
+
+    // return the value returned by the above renameAttribute() call
 }
